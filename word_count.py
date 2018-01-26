2cdd4723ff834bfb38b64c4f349bed890f06c028
Ritesh Dash <riteshdashcfc1236@gmail.com>
2018-01-26 14:27:24 +0530

    Add files via upload

---
+++ word_count.py
@@ -0,0 +1,18 @@
+
+import math
+
+dict={}
+
+str=" he said " 
+ 
+for i in range(0,len(str)-1):
+    if str[i+1]=="," or str[i+1]=="." or str[i+1]=="\"":
+        str=str[:i+1]+" "+","+" "+str[i+2:]
+
+for word in str.split():
+    if word in dict.keys():
+        dict[word]+=1
+    else:
+        dict[word]=1
+    
+print dict
