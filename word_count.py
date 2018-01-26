0da501c7a9a48d1c85030af0b0870b500f280f6d
Ritesh Dash <riteshdashcfc1236@gmail.com>
2018-01-26 14:31:41 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,18 +1,26 @@
 
+
+import sys
 import math
 
-dict={}
+fl=input("enter file :")
 
-str=" he said " 
- 
-for i in range(0,len(str)-1):
-    if str[i+1]=="," or str[i+1]=="." or str[i+1]=="\"":
-        str=str[:i+1]+" "+","+" "+str[i+2:]
+dict={} #dictionary to store frequencies...!!!
 
-for word in str.split():
-    if word in dict.keys():
-        dict[word]+=1
-    else:
-        dict[word]=1
-    
+with open(fl,'r') as f:
+    for line in f:
+        str=line
+        for i in range(0,len(str)-1 ):
+            if str[i+1]=="," or str[i+1]=="." or str[i+1]=="\"" or str[i+1]=="-" or str[i+1]=="!" : #to check special characters...!!!
+                str=str[:i+1]+" , "+str[i+2:]
+               
+        for word in str.split():
+            if word in dict.keys():
+                dict[word]+=1
+            else:
+                dict[word]=1
+
+
 print dict
+
+
